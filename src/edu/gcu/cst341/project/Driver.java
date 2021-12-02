bc9faaee96812b8baf739553a08f3c2d9096d4f7
Robert Loy (GCU) <robert.loy@gcu.edu>
2021-12-01 19:22:46 -0700

    Update Driver.java

--- src/edu/gcu/cst341/project/Driver.java
+++ src/edu/gcu/cst341/project/Driver.java
@@ -8,28 +8,27 @@ public class Driver {
 
 	}
 
-//Allen Craig
+	//Allen Craig
 	// practice GitHub 2021Dec01
 	public static void doAllen() {
 		System.out.println("Allen Craig");
-	}//
+	}
 
 
- //Madalina Marian
+	//Madalina Marian
 	//Practice GitHub 2021-Dec-01
-	
 	public static void Madalina() {
 		System.out.println("Madalina");
-
-  }
-//Desiree Mealey-Vasquez
+	}
+	
+	//Desiree Mealey-Vasquez
 	//Practice GitHub 12.01.2021
 	public static void doDez() {
 		System.out.println("Desiree Mealey-Vasquez");
 	}
-// Matt Kollar
-// Practice GitHub - 12/1/2021
 	
+	// Matt Kollar
+	// Practice GitHub - 12/1/2021
 	public static void doMatt() {
 		System.out.println("My name is Matt");
 
